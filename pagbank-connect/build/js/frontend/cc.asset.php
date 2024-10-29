0519ab8b5b6a5dafc8d194655f758aaa2fbcf166
Sean Murphy <sean@iamseanmurphy.com>
2024-10-29 04:31:54 +0000

    4.23.0

--- pagbank-connect/build/js/frontend/cc.asset.php
+++ pagbank-connect/build/js/frontend/cc.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wc-blocks-registry', 'wc-settings', 'wp-element', 'wp-html-entities', 'wp-i18n'), 'version' => '25f0982b066d328fdd3b');
+<?php return array('dependencies' => array('react', 'wc-blocks-registry', 'wc-settings', 'wp-element', 'wp-html-entities', 'wp-i18n'), 'version' => 'b68665eb85f001d5033c');
