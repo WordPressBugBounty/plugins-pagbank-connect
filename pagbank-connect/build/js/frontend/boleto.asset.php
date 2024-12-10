7af91dbfa79e4f79bc590adcfdd399afd51ea1b3
Sean Murphy <sean@iamseanmurphy.com>
2024-12-10 02:44:45 +0000

    4.25.3

--- pagbank-connect/build/js/frontend/boleto.asset.php
+++ pagbank-connect/build/js/frontend/boleto.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wc-blocks-registry', 'wc-settings', 'wp-element', 'wp-html-entities', 'wp-i18n'), 'version' => '02a3be0aa58c5b855f04');
+<?php return array('dependencies' => array('react', 'wc-blocks-registry', 'wc-settings', 'wp-element', 'wp-html-entities', 'wp-i18n'), 'version' => '8ce05f1e833c85db4bbf');
--- pagbank-connect/build/js/frontend/pix.asset.php
+++ pagbank-connect/build/js/frontend/pix.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wc-blocks-registry', 'wc-settings', 'wp-element', 'wp-html-entities', 'wp-i18n'), 'version' => 'bbc8c36402f160996754');
+<?php return array('dependencies' => array('react', 'wc-blocks-registry', 'wc-settings', 'wp-element', 'wp-html-entities', 'wp-i18n'), 'version' => '0f6bbcb16ba0878a0c59');
